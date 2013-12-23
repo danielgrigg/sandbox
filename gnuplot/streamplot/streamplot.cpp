27fefdc964128a8e2c4450e59853a510879831db
Daniel C Grigg <daniel.grigg@covata.com>
2013-12-23 13:51:43 +1100

    gnuplot

---
+++ gnuplot/streamplot/streamplot.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <sstream>
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  srandom(0);
+
+  while (true)
+  {
+
+  }
+  return 0;
+}
