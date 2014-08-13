2ebcb61f5d3b145c3efcff9c480ef9466748c126
Daniel Grigg <mail@danielgrigg.com>
2014-08-13 20:48:02 +1000

    improve the bitwise coding/decoding

--- langs/haskell/base64/hbase64.hs
+++ langs/haskell/base64/hbase64.hs
@@ -1,5 +1,7 @@
 import qualified Data.Map.Strict as M
 import Data.Bits
+import Data.Char
+import Data.List.Split
 
 encodings = M.fromList $
               zip [0..63] $ 
@@ -7,10 +9,15 @@ encodings = M.fromList $
 
 encodeValue k = M.lookup k encodings
 
-  
-rint2bits 0 = [0]
-rint2bits 1 = [1]
-rint2bits n = (n .&. 1) : (rint2bits (n `shiftR` 1))
+int2bits n = [ testBit n i | i <- [7,6..0]]
 
-int2bits :: Int -> [Int]
-int2bits = reverse . rint2bits 
+bits2int bs = 
+  let maxBit = length bs - 1
+      bitIndex = zip bs [maxBit,maxBit-1..0]
+  in sum $ 
+      map (\(isSet,i) -> if isSet then bit i else 0) bitIndex
+       
+
+char2bits = int2bits . ord
+string2bits s = foldl (++) [] $ map char2bits s
+
