16d3f073dba453cccaad18196e19b852fce7fde2
Daniel C Grigg <daniel.grigg@covata.com>
2014-08-14 11:23:15 +1000

    haskell - Create the base64 encode method

--- langs/haskell/base64/hbase64.hs
+++ langs/haskell/base64/hbase64.hs
@@ -2,15 +2,21 @@ import qualified Data.Map.Strict as M
 import Data.Bits
 import Data.Char
 import Data.List.Split
+import Data.Maybe
 
+-- base64 encoding table
 encodings = M.fromList $
               zip [0..63] $ 
                 ['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ ['+','/']
 
+-- encode a 6-bit value in base64
 encodeValue k = M.lookup k encodings
 
-int2bits n = [ testBit n i | i <- [7,6..0]]
+-- convert a 8-bit value to a list of bit flags
+byte2bits n = [ testBit n i | i <- [7,6..0]]
 
+-- convert a list of bit flags to an integer value
+bits2int::[Bool] -> Int
 bits2int bs = 
   let maxBit = length bs - 1
       bitIndex = zip bs [maxBit,maxBit-1..0]
@@ -18,6 +24,16 @@ bits2int bs =
       map (\(isSet,i) -> if isSet then bit i else 0) bitIndex
        
 
-char2bits = int2bits . ord
+char2bits = byte2bits . ord
 string2bits s = foldl (++) [] $ map char2bits s
 
+-- replace last n values  with r
+replaceLast n r s = (take (length s - n) s) ++ (replicate n r)
+
+encodePadded s = map (fromJust . encodeValue . bits2int) $ (chunksOf 6) . string2bits $ s
+
+leftover s n = length s `mod` n
+
+encode s | leftover s 3 == 1  = replaceLast 2 '=' $ encodePadded $ s ++ "\0\0"
+encode s | leftover s 3 == 2  = replaceLast 1 '=' $ encodePadded $ s ++ "\0"
+encode s = encodePadded s
