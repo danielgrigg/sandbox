3404da9eb1132d3c5086e93798eb3d368630abc4
Daniel Grigg <mail@danielgrigg.com>
2014-09-25 21:39:20 +1000

    Add rest of week2

--- langs/haskell/cs194/week2/LogAnalysis.hs
+++ langs/haskell/cs194/week2/LogAnalysis.hs
@@ -32,15 +32,15 @@ insert message@(LogMessage _ t _) (Node left node@(LogMessage _ tNode _) right) 
     else (Node left node (insert message right))
 insert message (Node _ (Unknown _) _) = Node Leaf message Leaf
 
-{-l0 = parseMessage "E 2 562 help help"-}
-{-l1 = parseMessage "I 29 la la la"-}
-{-l2 = parseMessage "This is not a message"-}
-ls = [LogMessage Info 30 "30", 
-      LogMessage Info 20 "20",
-      LogMessage Info 10 "10",
-      LogMessage Info 15 "15",
-      LogMessage Info 50 "50",
-      LogMessage Info 40 "40"]
+{-ls = [LogMessage Info 30 "30", -}
+      {-LogMessage Info 20 "20",-}
+      {-Unknown "foo",-}
+      {-LogMessage (Error 20) 99 "99",-}
+      {-LogMessage (Error 70) 70 "70",-}
+      {-LogMessage Info 10 "10",-}
+      {-LogMessage Info 15 "15",-}
+      {-LogMessage Info 50 "50",-}
+      {-LogMessage Info 40 "40"]-}
 
 build :: [LogMessage] -> MessageTree
 build messages = buildTree messages Leaf
@@ -48,4 +48,24 @@ build messages = buildTree messages Leaf
 buildTree :: [LogMessage] -> MessageTree -> MessageTree
 buildTree [] tree = tree
 buildTree (message:rest) tree = buildTree rest (insert message tree)
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left message right) = 
+  inOrder left ++ [message] ++ inOrder right
+
+orderLogs :: [LogMessage] -> [LogMessage]
+orderLogs = inOrder . build
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = (map getMessage) . filterError50 . orderLogs
+
+getMessage :: LogMessage -> String
+getMessage (LogMessage _ _ message) = message
+getMessage (Unknown message) = message
+
+filterError50 :: [LogMessage] -> [LogMessage]
+filterError50 [] = []
+filterError50 (m@(LogMessage (Error severity) _ _):rest)
+  | severity >= 50 = m:(filterError50 rest)
+filterError50 (_:rest) = filterError50 rest
 
