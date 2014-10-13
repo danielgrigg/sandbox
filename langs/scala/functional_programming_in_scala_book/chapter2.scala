c3af92967e8e9c2fbea633ae2e8b8090d07c679f
Daniel Grigg <mail@danielgrigg.com>
2014-10-13 20:58:33 +1100

    add chapter2 scala stuff

--- langs/scala/functional_programming_in_scala_book/chapter2.scala
+++ langs/scala/functional_programming_in_scala_book/chapter2.scala
@@ -7,4 +7,26 @@ object Chapter2 {
       else go(value, value+prev, n-1)
     go(0, 1, n-1)
   }
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = 
+      if (n + 1 >= as.length) true
+      else if (!ordered(as(n),as(n+1))) false
+      else loop(n+1)
+    loop(0)
+  }
+
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+    a:A => 
+      b:B => f(a,b)
+  }
+
+  def uncurry[A,B,C](f: A => B => C):(A, B) => C = {
+    (a:A, b:B) => f(a)(b)
+  }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a:A => f(g(a))
+  }
 }
